d78571842b270f1b40d93e13cb8ec860d15d2d69
colin8930 <colin8930@gmail.com>
2015-03-03 13:26:29 +0800

    non recursive (re-upload)

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,33 +11,21 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	mov r3, #-1	@pervios
+	mov r4, #1	@result
+	mov r5, #0	@i
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+.loop:
+	
+	add r6, r4, r3	@R6 = R4 + R3 sum=result+previous 
+	mov r3, r4 	@previous = result
+	mov r4, r6 	@result = sum
+	subs r0, #1 	@i++
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	ite lt
+	movlt r0 ,r4
+	bge .loop 	@when i<=x, continue the loop
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
+	bx lr
 	.size fibonacci, .-fibonacci
 	.end
