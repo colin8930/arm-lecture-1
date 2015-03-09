51c8e804d5ec610c3e8025b4d84b372d67117a65
colin8930 <colin8930@gmail.com>
2015-03-09 20:03:07 +0800

    recording time via gdb

--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
 
@@ -7,8 +8,13 @@ int main(int argc, char **argv)
   int number=0;
   int result=0;
 
-  scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
-}
+  if( scanf("%d",&number) == 1 ) {
+	clock_t t = clock();
+  	result = fibonacci(number);   
+	t = clock() - t;
+	printf("Clock : %d\n", t);
+  	printf("The fibonacci sequence at %d is: %d\n", number, result);
+ }
 
+ return result;
+}
